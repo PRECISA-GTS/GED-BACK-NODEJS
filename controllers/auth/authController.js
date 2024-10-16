0e8a74636272c879e56bf232bfe3a30e69f679e1
robertodelavi <roberto.delavy@gmail.com>
2024-10-16 15:05:54 -0300

    Auth ok

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -124,6 +124,101 @@ class AuthController {
             return res.status(500).json({ message: err.message });
         }
     }
+
+    async getAvailableRoutes(req, res) {
+        const functionName = req.headers['function-name'];
+        const { usuarioID, unidadeID, papelID } = req.query;
+
+        // Menu e Routes
+        switch (functionName) {
+            case 'getMenu':
+                const menu = await getMenu(papelID)
+
+                res.status(200).json(menu);
+                break;
+
+            case 'getRoutes':
+                let sqlRoutes = ``
+                const admin = req.query.admin;
+                //? Usuário admin ou fornecedor, acessa todas as rotas do seu papel
+                if (admin == 1 || papelID == 2) {
+                    sqlRoutes = `
+                    SELECT IF(m.rota <> '', m.rota, s.rota) AS rota, 1 AS ler, 1 AS inserir, 1 AS editar, 1 AS excluir
+                    FROM divisor AS d 
+                        JOIN menu AS m ON (d.divisorID = m.divisorID)  
+                        LEFT JOIN submenu AS s ON (m.menuID = s.menuID)
+                    WHERE d.papelID = ${papelID} AND m.status = 1 OR s.status = 1`
+                } else {
+                    // Não é admin, busca permissões da tabela permissao
+                    sqlRoutes = `
+                    SELECT rota, papelID, ler, inserir, editar, excluir
+                    FROM permissao                    
+                    WHERE papelID = ${papelID} AND usuarioID = ${usuarioID} AND unidadeID = ${unidadeID}`;
+                }
+
+                db.query(sqlRoutes, (err, result) => {
+                    if (err) { return res.status(500).json({ message: err.message }); }
+
+                    result.forEach(rota => {
+                        rota.ler = rota.ler === 1 ? true : false;
+                        rota.inserir = rota.inserir === 1 ? true : false;
+                        rota.editar = rota.editar === 1 ? true : false;
+                        rota.excluir = rota.excluir === 1 ? true : false;
+                    })
+
+                    res.status(200).json(result);
+                })
+
+                break;
+        }
+    }
+
+    //? Função que valida se o CPF é válido e retorna o mesmo para o front / para redefinir senha
+    async routeForgotEmailValidation(req, res) {
+        const { data } = req.body;
+        const type = req.query.type;
+
+        if (type == 'login') {
+            let sql = `SELECT * FROM usuario WHERE cpf = ?`;
+            const [result] = await db.promise().query(sql, [data]);
+            res.status(200).json(result[0]);
+        } else if (type == 'fornecedor') {
+            let sql = `SELECT email, nome, usuarioID FROM usuario WHERE cnpj = ?`;
+            const [result] = await db.promise().query(sql, [data]);
+            res.status(200).json(result[0] ? result[0] : null);
+        } else {
+            res.status(400).json({ message: 'Essa rota não é válida!' });
+        }
+    }
+
+    //? Função que recebe os dados e envia o email com os dados de acesso
+    async forgotPassword(req, res) {
+        const { data } = req.body;
+        const type = req.query.type;
+
+        let assunto = 'Redefinir senha'
+
+        const values = {
+            nome: data.nome,
+            usuarioID: data.usuarioID,
+            type: type,
+            noBaseboard: true,
+        }
+
+        const html = await NewPassword(values)
+        res.status(200).json(sendMailConfig(data.email, assunto, html));
+    }
+
+    //? Função que redefine a senha do usuário
+    async routeForgotNewPassword(req, res) {
+        const { data } = req.body;
+        const logID = await executeLog('Redefinir senha', data.usuarioID, 1, req)
+
+        let sql = `UPDATE usuario SET senha = ? WHERE usuarioID = ?`;
+        await executeQuery(sql, [criptoMd5(data.senha), data.usuarioID], 'update', 'usuario', 'usuarioID', data.usuarioID, logID)
+
+        return res.status(200).json({ message: 'Senha alterada com sucesso!' });
+    }
 }
 
 module.exports = AuthController;
--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
 const router = Router();
 const route = '/login';
+const routeForgotPassword = '/esqueceuSenha';
 
 const AuthController = require('../../controllers/auth/authController');
 const authController = new AuthController();
+
 router.post(`${route}`, authController.login);
+router.get(`${route}`, authController.getAvailableRoutes);
+router.post(`${routeForgotPassword}`, authController.forgotPassword);
+router.post(`${routeForgotPassword}/validation`, authController.routeForgotEmailValidation);
+router.post(`${routeForgotPassword}/newPassword`, authController.routeForgotNewPassword);
 
 module.exports = router;
